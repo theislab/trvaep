532eb60efdb9fa440261e96d8f74b3bcadc7a28f
m0hammadl <mohammad.lotfollahi@helmholtz-muenchen.de>
2019-12-11 00:17:16 +0100

    Add pyproject.toml

--- trvaep/__init__.py
+++ trvaep/__init__.py
@@ -1,2 +1,20 @@
+"""trVAE - Transfer Variational Autoencoders pytorch"""
+
 from .model import *
 from .data_loader import CustomDatasetFromAdata
+
+
+
+
+__author__ = ', '.join([
+    'Mohammad Lotfollahi'
+])
+
+__email__ = ', '.join([
+    'Mohammad.lotfollahi@helmholtz-muenchen.de',
+])
+
+from get_version import get_version
+__version__ = get_version(__file__)
+
+del get_version

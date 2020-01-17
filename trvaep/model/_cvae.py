ffd64406907a99e02262fc6eda79269be8db10e6
m0hammadl <mohammad.lotfollahi@helmholtz-muenchen.de>
2020-01-17 17:52:31 +0100

    Update files

--- trvaep/model/_cvae.py
+++ trvaep/model/_cvae.py
@@ -35,9 +35,9 @@ class CVAE(nn.Module):
 
         """
 
-    def __init__(self, input_dim, num_classes=None, encoder_layer_sizes=[64],
-                 latent_dim=10, decoder_layer_sizes=[64], alpha=0.001, use_batch_norm=True,
-                 dr_rate=0.2, use_mmd=False, beta=1, output_activation="RelU"):
+    def __init__(self, input_dim, num_classes=None, encoder_layer_sizes=[128, 32],
+                 latent_dim=10, decoder_layer_sizes=[32, 128], alpha=0.001, use_batch_norm=True,
+                 dr_rate=0.2, use_mmd=False, beta=10, output_activation="RelU"):
         super().__init__()
         assert type(encoder_layer_sizes) == list
         assert type(latent_dim) == int
--- trvaep/torch_two_sample/__init__.py
+++
@@ -1,6 +0,0 @@
-from .statistics_diff import (
-    SmoothFRStatistic, SmoothKNNStatistic, MMDStatistic, EnergyStatistic)
-from .statistics_nondiff import FRStatistic, KNNStatistic
-
-__all__ = ['SmoothFRStatistic', 'SmoothKNNStatistic', 'MMDStatistic',
-           'EnergyStatistic', 'FRStatistic', 'KNNStatistic']
